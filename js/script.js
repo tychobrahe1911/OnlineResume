ec3856c3e754368c07a894ddf8914785046aef93
R G <rgyde@stackpoleengineering.com>
2024-03-11 09:44:48 -0400

    start of email protection

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // Make mobile navigation work
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+// const btnNavEl = document.querySelector(".btn-mobile-nav");
+// const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
+// btnNavEl.addEventListener("click", function () {
+//   headerEl.classList.toggle("nav-open");
+// });
 
+var encEmail = "cnNneWRlQG10dS5lZHU=";
+const form = document.getElementById("contactEmail");
+// form.setAttribute("href", "mailto:".concat(atob(encEmail)));
+form.setAttribute("href", "mailto: rsgyde@mtu.edu");
